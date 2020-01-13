737f907598399965c0fc806eb59eb8af83318609
Théo Bernard <theo.bernard.94100@gmail.com>
2020-01-13 10:02:16 +0100

    Roll a ball Assets

---
+++ Scripts/MainMenu.js
@@ -0,0 +1,29 @@
+﻿#pragma strict
+
+function START_THE_GAME () {
+	Application.LoadLevel("Level1");
+}
+
+function QUIT_THE_GAME () {
+	Application.Quit();
+}
+
+function VERS_LEVEL2 () {
+	Application.LoadLevel("Level2");
+}
+
+function VERS_STAGE_MENU () {
+	Application.LoadLevel("Stage Menu");
+}
+
+function VERS_LEVEL3 () {
+	Application.LoadLevel("Level3");
+}
+
+function VERS_LEVEL4 () {
+	Application.LoadLevel("Level4");
+}
+
+function VERS_LEVEL5 () {
+	Application.LoadLevel("Level5");
+}
---
+++ Scripts/PlayerController.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour {
+
+	public int speed = 500;
+	public int count;
+	public GUIText countText;
+
+	// Use this for initialization
+	void Start () 
+	{
+		count = 0;
+		SetCountText();
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate () 
+	{
+		float mouveHorizontal = Input.GetAxis ("Horizontal");
+		float mouveVertical = Input.GetAxis ("Vertical");
+
+		Vector3 mouvement = new Vector3(mouveHorizontal, 0, mouveVertical);
+		GetComponent<Rigidbody>().AddForce(mouvement * speed * Time.deltaTime);
+	}
+
+	void OnTriggerEnter (Collider other)
+	{
+		if (other.gameObject.tag == "Cube") {
+			other.gameObject.SetActive (false);
+			count++;
+			SetCountText ();
+		} else if (other.gameObject.tag == "Killzone") {
+			Application.LoadLevel (1);
+		} else if (other.gameObject.tag == "Killzone2") {
+			Application.LoadLevel (3);
+		} else if (other.gameObject.tag == "Killzone3") {
+			Application.LoadLevel (4);
+		} else if (other.gameObject.tag == "Killzone4") {
+			Application.LoadLevel (5);
+		} else if (other.gameObject.tag == "Killzone5") {
+			Application.LoadLevel (6);
+		}
+
+	}
+	void SetCountText()
+	{
+		countText.text = "Score : " + count.ToString ();
+
+		if(count >= 10)
+		{
+			Application.LoadLevel (2);
+		}
+	}
+}
